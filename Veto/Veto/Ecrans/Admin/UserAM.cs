90316e29204ee5d28caf1f28efa2fd795b911152
Léo PUYASTIER <leo.puyastier@etu.u-bordeaux.fr>
2022-02-12 11:51:15 +0100

    Début de l'arbrescence + Ecrans admins prêts

---
+++ Veto/Veto/Ecrans/Admin/UserAM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Veto.Ecrans.Admin
+{
+    public partial class UserAM : Veto.Ecrans.Admin.UserBase
+    {
+        public UserAM()
+        {
+            InitializeComponent();
+        }
+    }
+}
