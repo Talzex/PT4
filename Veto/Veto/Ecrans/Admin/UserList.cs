c34a67e7123cbf42d9ec97bde8b25e86595c8bfd
Puyastier Leo <leo.puyastier@etu.u-bordeaux.fr>
2022-02-14 10:43:17 +0100

    Arborescence projet

---
+++ Veto/Veto/Ecrans/Admin/UserList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Veto
+{
+    public partial class UserList : Veto.EcranAdmin
+    {
+        public UserList()
+        {
+            InitializeComponent();
+        }
+    }
+}
