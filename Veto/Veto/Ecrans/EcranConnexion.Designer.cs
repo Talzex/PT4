ec7b13e55129758457033ad21786c5ef0a5eef29
Puyastier Leo <leo.puyastier@etu.u-bordeaux.fr>
2022-02-18 11:00:51 +0100

    Ecran base

--- Veto/Veto/Ecrans/EcranConnexion.Designer.cs
+++ Veto/Veto/Ecrans/EcranConnexion.Designer.cs
@@ -29,9 +29,100 @@ namespace Veto
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(EcranConnexion));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.LoginTB = new System.Windows.Forms.TextBox();
+            this.PasswordTB = new System.Windows.Forms.TextBox();
+            this.ConnectBT = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(9)))), ((int)(((byte)(51)))), ((int)(((byte)(39)))));
+            this.panel1.Controls.Add(this.ConnectBT);
+            this.panel1.Controls.Add(this.PasswordTB);
+            this.panel1.Controls.Add(this.LoginTB);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(88, 233);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(235, 262);
+            this.panel1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(224)))), ((int)(((byte)(204)))));
+            this.label1.Location = new System.Drawing.Point(62, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Connexion";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // LoginTB
+            // 
+            this.LoginTB.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.LoginTB.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(224)))), ((int)(((byte)(204)))));
+            this.LoginTB.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.LoginTB.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LoginTB.Location = new System.Drawing.Point(58, 96);
+            this.LoginTB.Name = "LoginTB";
+            this.LoginTB.Size = new System.Drawing.Size(115, 22);
+            this.LoginTB.TabIndex = 1;
+            this.LoginTB.Text = "Identifiant";
+            this.LoginTB.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // PasswordTB
+            // 
+            this.PasswordTB.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.PasswordTB.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(224)))), ((int)(((byte)(204)))));
+            this.PasswordTB.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.PasswordTB.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PasswordTB.Location = new System.Drawing.Point(59, 152);
+            this.PasswordTB.Name = "PasswordTB";
+            this.PasswordTB.Size = new System.Drawing.Size(115, 22);
+            this.PasswordTB.TabIndex = 2;
+            this.PasswordTB.Text = "Mot de passe";
+            this.PasswordTB.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // ConnectBT
+            // 
+            this.ConnectBT.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ConnectBT.FlatAppearance.BorderSize = 0;
+            this.ConnectBT.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ConnectBT.ForeColor = System.Drawing.Color.Black;
+            this.ConnectBT.Location = new System.Drawing.Point(68, 205);
+            this.ConnectBT.Name = "ConnectBT";
+            this.ConnectBT.Size = new System.Drawing.Size(96, 28);
+            this.ConnectBT.TabIndex = 3;
+            this.ConnectBT.Text = "Connexion";
+            this.ConnectBT.UseVisualStyleBackColor = false;
+            // 
+            // EcranConnexion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1064, 681);
+            this.Controls.Add(this.panel1);
+            this.Name = "EcranConnexion";
+            this.Controls.SetChildIndex(this.panel1, 0);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button ConnectBT;
+        private System.Windows.Forms.TextBox PasswordTB;
+        private System.Windows.Forms.TextBox LoginTB;
+        private System.Windows.Forms.Label label1;
     }
 }
