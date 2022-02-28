feb374004418bf74af85e737ede3a17f672081c0
lunouhaud <lucas.nouhaud@etu.u-bordeaux.fr>
2022-02-28 11:55:21 +0100

    ajout infos sur ClientDetails

--- Veto/Veto/Screen/Employee/ClientsDetails.Designer.cs
+++ Veto/Veto/Screen/Employee/ClientsDetails.Designer.cs
@@ -37,6 +37,8 @@
             this.AddAnimalButton = new System.Windows.Forms.Button();
             this.ModifyClient = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // LastNameLabel
@@ -138,12 +140,34 @@
             this.button1.Text = "Supprimer";
             this.button1.UseVisualStyleBackColor = false;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(22, 369);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 21);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Nombre:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(186, 369);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 21);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Remise:";
+            // 
             // ClientsDetails
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(186)))), ((int)(((byte)(219)))), ((int)(((byte)(209)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.ModifyClient);
             this.Controls.Add(this.AddAnimalButton);
@@ -171,5 +195,7 @@
         private System.Windows.Forms.Button AddAnimalButton;
         private System.Windows.Forms.Button ModifyClient;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
