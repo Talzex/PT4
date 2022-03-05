b3647e0939cbb8cb314f0a968453ae7d678d02f0
Léo PUYASTIER <leo.puyastier@etu.u-bordeaux.fr>
2022-03-05 19:30:03 +0100

    Création du design de présentation d'un salarié

---
+++ Veto/Veto/DBEntities/MyRole.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Veto
+{
+    public partial class Roles
+    {
+        public override string ToString()
+        {
+            return this.Nom;
+        }
+    }
+}
---
+++ Veto/Veto/UserControls/UserDesign.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Veto
+{
+    public partial class UserDesign : UserControl
+    {
+
+        private Salarie employee;
+
+        public UserDesign(Salarie employee)
+        {
+            InitializeComponent();
+            this.employee = employee;
+            DescLBL.Text = employee.Login + "\t| " + employee.Roles;
+        }
+    }
+}
